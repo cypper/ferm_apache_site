7a627151b26caf5eb66a847b9d1540e60880920d
Nazar Pokora <cypper1999@gmail.com>
2018-02-27 16:42:20 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,16 +4,13 @@ date_default_timezone_set("Europe/Kiev");
 
 
 define("MG_DB", "localhost");
-define("MG_NAME", "fermua");
-define("MG_PASSWORD", urlencode("xDH&65rNrh@Yzy+s"));
+define("MG_NAME", "asd");
+define("MG_PASSWORD", urlencode("xDH&6weNrh@Yzy+s"));
 
 define("MQ_HOST", "localhost");
-define("MQ_DB", "main");
-define("MQ_NAME", "root");
-define("MQ_PASSWORD", "cypper1999");
-// define("MQ_DB", "fermppua_main");
-// define("MQ_NAME", "fermppua_main");
-// define("MQ_PASSWORD", "~8ZkgPK^*ac[");
+define("MQ_DB", "awgere");
+define("MQ_NAME", "aewrger");
+define("MQ_PASSWORD", "asdfawe");
 
 define("DATE_FORMAT", "d M Y H:i:s");
 
